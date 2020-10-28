17b2f91674093a76deeec48257935e9b65e32a58
Ola Ouso <ouso.ola@gmail.com>
2020-10-28 18:44:22 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  // why is it not working when it's not an arrow function
+  handleClick = () => {
+    this.setState({timesClicked: this.state.timesClicked+1})
+  }
+
+  render() {
+    return (
+    <button
+    onClick = {this.handleClick}
+    >{this.state.timesClicked}</button>
+    )
+  }
+}
+
+export default DigitalClicker 
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,57 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+class YouTubeDebugger extends Component {
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+
+	handleClickOnBitrate = () => {
+
+		this.setState( {
+				settings: {
+					...this.state.settings,
+					bitrate: 12
+				}
+			}
+		)
+	}
+
+	handleClickOnResolution = () => {
+		this.setState ({
+			settings: {
+				...this.state.settings,
+				video: {
+          resolution: '720p'
+        }
+			}
+		})
+	}
+  render() {
+		console.log(this.state);
+    return (
+			<React.Fragment>
+      <button className = "bitrate" onClick = {this.handleClickOnBitrate}>bitrate</button>
+			<button
+			className = "resolution"
+			onClick = {this.handleClickOnResolution}
+			>resolution</button>
+			</React.Fragment>
+    )
+  }
+
+}
+
+
+export default YouTubeDebugger
